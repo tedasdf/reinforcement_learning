416450d7877805e3963528977a634971a4f5d58c
tedasdf <154312357+tedasdf@users.noreply.github.com>
2026-01-18 20:35:43 +1100

    Update DDPGagent.py

--- rl_project_new/agents/DDPGagent.py
+++ rl_project_new/agents/DDPGagent.py
@@ -1,17 +1,24 @@
 import torch
-
+import copy
 from rl_project_new.agents.base import BaseAgent
 from rl_project_new.agents.utils import replay_to_tensor
 from rl_project_new.buffer.replay_buffer import ReplayBuffer
 
 
 class DDPGnetRLAgent(BaseAgent):
-    def __init__(self, replay_buffer: ReplayBuffer, network, gamma, critic_lr, actor_lr , device, action_space):
+    def __init__(self, replay_buffer: ReplayBuffer, network, gamma, critic_lr, actor_lr , device, action_space, target_network, tau):
         super().__init__(device, network, action_space)
         self.replay_buffer = replay_buffer
         self.gamma = gamma
         self.actor_lr = actor_lr
         self.critic_lr = critic_lr
+        self.target_network = copy.deepcopy(self.network)
+        self.tau = tau
+    
+    def soft_update(self):
+        for p, p_targ in zip(self.network.parameters(), self.target_network.parameters()):
+            p_targ.data.mul_(1 - self.tau)
+            p_targ.data.add_(self.tau * p.data)
 
     def setup_network(self):
 
@@ -19,7 +26,6 @@ class DDPGnetRLAgent(BaseAgent):
             'actor':    torch.optim.Adam(self.network.parameters(), lr=self.actor_lr),
             'critic':   torch.optim.Adam(self.network.parameters(), lr=self.critic_lr),
         }
-
     
     def get_action(self, state_tensor):
         action, value = self.network(state_tensor)
@@ -35,7 +41,7 @@ class DDPGnetRLAgent(BaseAgent):
     replay_to_tensor(*self.replay_buffer.sample(), device=self.device)
 
         with torch.no_grad():
-            _, next_q_values_target = self.network(next_states_tensor)
+            _, next_q_values_target = self.target_network(next_states_tensor)
             target_q_values = rewards_tensor + self.gamma * (1 - dones_tensor) * next_q_values_target
 
         return target_q_values, states_tensor, actions_tensor
@@ -53,8 +59,9 @@ class DDPGnetRLAgent(BaseAgent):
 
         critic_loss = self.network.loss(current_q_values, target_q_values)
 
-        value_tensors, _ = self.network(states_tensor)
+        value_tensors, _ = self.network(states_tensor, use_noise=False)
         actor_loss = -value_tensors.mean()
+
         return {
             'critic_loss': critic_loss,
             'actor_loss': actor_loss
