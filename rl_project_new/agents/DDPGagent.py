c3644779d0f806a5775c24f1b2482b060d206ebb
tedasdf <154312357+tedasdf@users.noreply.github.com>
2026-01-20 17:04:25 +1100

    Update DDPGagent.py

--- rl_project_new/agents/DDPGagent.py
+++ rl_project_new/agents/DDPGagent.py
@@ -15,6 +15,8 @@ class DDPGnetRLAgent(BaseAgent):
         self.critic_lr = critic_lr
         self.target_network = copy.deepcopy(self.network)
         self.tau = tau
+        self.network = self.network.to(device)
+        self.target_network = self.target_network.to(device)
     
     def soft_update(self):
         with torch.no_grad():
