f2c7161c44bd22d93db67b4eab1d0a58fae5943e
tedasdf <154312357+tedasdf@users.noreply.github.com>
2026-01-20 17:03:23 +1100

    Update DDPG.py

--- rl_project_new/algorithms/DDPG.py
+++ rl_project_new/algorithms/DDPG.py
@@ -91,7 +91,8 @@ class OUActionNoise():
 
     def __call__(self):
         x = self.x_prev + self.theta * (self.mu - self.x_prev) * self.dt + self.sigma * np.sqrt(self.dt) * np.random.normal(size=self.mu.shape)
-    
+        self.x_prev = x 
+        return x
     def reset(self):
         self.x_prev = self.x0 if self.x0 is not None else np.zeros_like(self.mu)
 
