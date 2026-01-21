619a0e45d7817808c02d8f096571f9525f881364
tedasdf <154312357+tedasdf@users.noreply.github.com>
2026-01-21 20:54:44 +1100

    Update DDPG_example.py

--- rl_project_new/algorithms/DDPG_example.py
+++ rl_project_new/algorithms/DDPG_example.py
@@ -235,7 +235,8 @@ class Agent(object):
         self.update_network_parameters()
 
         print(critic_loss.shape)
-        raise ValueError
+        if critic_loss is None:
+            raise ValueError
         return critic_loss, actor_loss
 
     def update_network_parameters(self, tau=None):
