925e7067bd5902f55edd7c3b91ba9731586d301e
tedasdf <154312357+tedasdf@users.noreply.github.com>
2026-01-21 20:43:21 +1100

    Update utils.py

--- rl_project_new/agents/utils.py
+++ rl_project_new/agents/utils.py
@@ -1,18 +1,9 @@
 import torch
 
-def replay_to_tensor(states, actions, rewards, next_states, dones, device=None):
-    rewards_tensor = torch.tensor(rewards, dtype=torch.float)
-    dones_tensor = torch.tensor(dones)
-    next_states_tensor = torch.tensor(next_states, dtype=torch.float)
-    actions_tensor = torch.tensor(actions, dtype=torch.float)
-    states_tensor = torch.tensor(states, dtype=torch.float)
-
-
-    if device:
-        states_tensor = states_tensor.to(device)
-        actions_tensor = actions_tensor.to(device)
-        rewards_tensor = rewards_tensor.to(device)
-        next_states_tensor = next_states_tensor.to(device)
-        dones_tensor = dones_tensor.to(device)
-    
+def replay_to_tensor(states, actions, rewards, next_states, dones, device):
+    states_tensor = torch.tensor(states, dtype=torch.float, device=device)
+    actions_tensor = torch.tensor(actions, dtype=torch.float, device=device)
+    rewards_tensor = torch.tensor(rewards, dtype=torch.float, device=device)
+    next_states_tensor = torch.tensor(next_states, dtype=torch.float, device=device)
+    dones_tensor = torch.tensor(dones, dtype=torch.float, device=device)
     return states_tensor, actions_tensor, rewards_tensor, next_states_tensor, dones_tensor
